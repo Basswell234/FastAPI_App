23e227c9c134c0b2ec6bc917c746e908b9589325
Basswell Mbilima <basswellbiotechmbilima@gmail.com>
2026-09-06 23:19:58 +0200

    added pydantics

--- books2.py
+++ books2.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
@@ -17,6 +18,11 @@ class Book:
         self.description = description
         self.rating = rating
 
+class BookRequest(BaseModel):
+    title: str
+    author: str
+    description: str
+    rating: int
 
 BOOKS = [
     Book(1, 'Computer Science', 'CodingWithBass', 'Description One', 5),
@@ -31,7 +37,9 @@ BOOKS = [
 async def read_all_books():
     return BOOKS
 
+
 @app.post("/create-book")
-async def create_book(book_request=Body()):
-    BOOKS.append(book_request)
-    return book_request
+async def create_book(book_request: BookRequest):
+    new_book = Book(**book_request.model_dump())
+    BOOKS.append(new_book)
+    return new_book
